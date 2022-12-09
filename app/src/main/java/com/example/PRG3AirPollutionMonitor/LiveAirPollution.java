d65e6b8de344afe67e1033d25637daa33a7ff6da
j4ketaylor <115506035+j4ketaylor@users.noreply.github.com>
2022-12-09 03:09:30 +0000

    Lots of work on LiveAirPollution and some UI tweaks

--- app/src/main/java/com/example/PRG3AirPollutionMonitor/LiveAirPollution.java
+++ app/src/main/java/com/example/PRG3AirPollutionMonitor/LiveAirPollution.java
@@ -3,12 +3,124 @@ package com.example.PRG3AirPollutionMonitor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LiveAirPollution extends AppCompatActivity {
+
+    TextView location_live_viewer;
+
+    TextView air_pollution_rating_viewer;
+
+    String[] items = {
+            "Barking & Dagenham",
+            "Bexley",
+            "Brent",
+            "Brentwood",
+            "Bromley",
+            "Camden",
+            "Castle Point",
+            "City of London",
+            "Croydon",
+            "Dartford",
+            "Ealing",
+            "Enfield",
+            "Greenwich",
+            "Hackney",
+            "Haringey",
+            "Harrow",
+            "Havering",
+            "Islington",
+            "Kingston",
+            "Lambeth",
+            "Lewisham",
+            "Merton",
+            "Newham",
+            "Redbridge",
+            "Reigate & Banstead",
+            "Richmond",
+            "Sevenoaks",
+            "Southwark",
+            "Sutton",
+            "Thurrock",
+            "Tower Hamlets",
+            "Wansdsorth",
+            "Westminster",
+            "Windsor & Maidenhead",
+    };
+
+    Integer[] PolutionLevels = {
+            1,
+            2,
+            2,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+    };
+
+    AutoCompleteTextView autoCompleteTxt;
+
+    ArrayAdapter<String> adapterItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_air_pollution);
+
+        location_live_viewer = findViewById(R.id.location_live_viewer_text);
+        location_live_viewer.setText("Select a Borough");
+        air_pollution_rating_viewer = findViewById(R.id.air_pollution_rating_viewer_text);
+        air_pollution_rating_viewer.setText("Polution Level:");
+
+        autoCompleteTxt = findViewById(R.id.auto_complete_txt);
+
+        adapterItems = new ArrayAdapter<String>(this,R.layout.list_item,items);
+
+        autoCompleteTxt.setAdapter(adapterItems);
+
+        autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(),"Item: " +item,Toast.LENGTH_SHORT).show();
+                air_pollution_rating_viewer.setText("Polution Level in " + item + " : " + PolutionLevels[Arrays.asList(items).indexOf(item)]);
+            }
+        });
+
     }
 }
