24f8c5336cd3f8dbe457bf1be0cab51af96daaff
jikai13 <jw2520@ic.ac.uk>
2023-01-10 18:08:25 +0000

    Added some comments and cleaned up UI

--- app/src/main/java/com/example/PRG3AirPollutionMonitor/IndexInfo.java
+++ app/src/main/java/com/example/PRG3AirPollutionMonitor/IndexInfo.java
@@ -3,13 +3,14 @@ package com.example.PRG3AirPollutionMonitor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class IndexInfo extends AppCompatActivity {
 
-    TextView colour_text, index_description_text1, index_description_text2;
+    TextView colour_text, index_description_text1, index_description_text2, index_description_text3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +18,23 @@ public class IndexInfo extends AppCompatActivity {
         setContentView(R.layout.activity_index_info);
 
         colour_text = (TextView) findViewById(R.id.colour_key);
-        colour_text.setText(" \uD83D\uDFE2 Good?\n \uD83D\uDFE0 Dangerous?");
+        colour_text.setText(" \uD83D\uDFE2 Low Risk\n \uD83D\uDD34 High Risk");
 
         index_description_text1 = (TextView) findViewById(R.id.index_description1);
         index_description_text1.setText("The asthma index is used to quantify the effects of pollution for asthma sufferers.");
 
         index_description_text2 = (TextView) findViewById(R.id.index_description2);
-        index_description_text2.setText("Based off research, the following pollutants were weighted with respect to their properties to increase the risk of asthma attacks.\nThe asthma index is calculated using live data of Defra's Air Pollution Index provided by London Air's API.\n");
+        index_description_text2.setText("Based off research [1], the following pollutants " +
+                "were weighted with respect to their properties to increase the risk of asthma attacks." +
+                "\nThe asthma index is calculated using live data of Defra's Air Pollution Index [2] provided by London Air's API.\n");
+
+        index_description_text3 = (TextView) findViewById(R.id.index_description3);
+        String reference_links = "\nReferences: \n[1] Guarnieri M, Balmes JR. Outdoor " +
+                "air pollution and asthma. Lancet. 2014 May 3;383(9928):1581-92. doi: " +
+                "10.1016/S0140-6736(14)60617-6. PMID: 24792855; PMCID: PMC4465283\n[2] "+
+                "https://www.londonair.org.uk/london/asp/airpollutionindex.asp?la_id=&region=0&bulletin=hourly&site=&bulletindate=10/01/2023%2015:00:00&level=All&MapType=Google&VenueCode=\n";
+        index_description_text3.setText(reference_links);
+        Linkify.addLinks(index_description_text3,Linkify.WEB_URLS);
     }
+
 }
--- app/src/main/java/com/example/PRG3AirPollutionMonitor/PlotInfo.java
+++ app/src/main/java/com/example/PRG3AirPollutionMonitor/PlotInfo.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class PlotInfo extends AppCompatActivity {
 
-    TextView plot_description_text;
+    TextView plot_description_text, plot_description_text2;
 
     Button plot_info_to_index_button;
 
@@ -26,6 +27,18 @@ public class PlotInfo extends AppCompatActivity {
 
         plot_description_text = (TextView) findViewById(R.id.plot_description);
         plot_description_text.setText("The graphs are plotted using data from the London Air Quality Network Summary" +
-                " Reports between 2015 and 2020. Several areas are left out due to limited data avalibility in the interval.");
+                " Reports between 2015 and 2020 [1]. Several areas are left out due to limited data avalibility in" +
+                "the interval.\nThe graphs can be used to help with choosing where to live in London, however, we " +
+                "recommend asthma sufferers to live as far away as possible from major roadways [2].");
+
+        plot_description_text2 = (TextView) findViewById(R.id.plot_description2);
+        String reference_links1 = "\nReferences: \n[1]" + " https://www.londonair.org.uk/london/asp/reports.asp\n" +
+                "[2] Guarnieri M, Balmes JR. Outdoor " +
+                "air pollution and asthma. Lancet. 2014 May 3;383(9928):1581-92. doi: " +
+                "10.1016/S0140-6736(14)60617-6. PMID: 24792855; PMCID: PMC4465283\n";
+        plot_description_text2.setText(reference_links1);
+        Linkify.addLinks(plot_description_text2,Linkify.WEB_URLS);
+
+
     }
 }
